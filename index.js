ba41e343d43c6c2beeaffd9f435283fce4794ea5
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-06-06 17:28:22 +0100

    Set up initial classes

---
+++ index.js
@@ -0,0 +1,14 @@
+// Class to make and run methods on LinkedLists
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+}
+
+// Class to make Nodes
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
