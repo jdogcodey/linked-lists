8c575ce71e137ec6537a1008464c13d63ae6e3ff
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-06-06 17:31:19 +0100

    Added functions to fill-in

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+  append(value) {}
+
+  prepend(value) {}
+
+  size() {}
+
+  head() {}
+
+  tail() {}
+
+  at(index) {}
+
+  pop() {}
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {}
 }
 
 // Class to make Nodes
