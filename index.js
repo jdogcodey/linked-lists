1787ed92168342fc02f9df7bf31d3f5b9f00306a
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-06-06 18:05:07 +0100

    Add first few methods

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
-  append(value) {}
+  append(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+      return this;
+    } else {
+      this.tail().next = new Node(value);
+      return this;
+    }
+  }
 
-  prepend(value) {}
+  prepend(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+      return this;
+    } else {
+      const currentHead = this.head;
+      this.head = new Node(value, currentHead);
+      return this;
+    }
+  }
 
-  size() {}
+  size() {
+    let noOfNodes = 0;
+    let currentNode = this.head;
+    while (currentNode != null) {
+      noOfNodes++;
+      currentNode = currentNode.next;
+    }
+    return noOfNodes;
+  }
 
   head() {}
 
